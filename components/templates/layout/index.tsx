b4856c5bbfb28e58928f242feee1a412499f35e3
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-06-28 10:15:29 +0200

    Fix errors

--- components/templates/layout/index.tsx
+++ components/templates/layout/index.tsx
@@ -15,49 +15,43 @@ interface LayoutProps {
 const Layout = ({ children, footer }: LayoutProps) => {
   const { lang } = useContext(RootContext);
 
-  // const [data, setData] = useState();
+  const [data, setData] = useState();
 
-  let tagline = "abc";
+  let tagline;
 
-  // useEffect(() => {
-  //   console.log("in use effect start");
+  useEffect(() => {
+    if (
+      !process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ||
+      !process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY
+    )
+      console.log("nie ma danych z envów");
 
-  //   if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_KEY)
-  //     return;
-  //   console.log(
-  //     "in use effect start2",
-  //     process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-  //   );
-  //   console.log(
-  //     "in use effect start2",
-  //     process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY
-  //   );
+    const client = createClient({
+      space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
+    });
 
-  //   const client = createClient({
-  //     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  //     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
-  //   });
+    client
+      .getEntries({ content_type: "footer" })
+      .then((res) => {
+        setData(res.items);
+        console.log("in use effect", res.items[0]);
+      })
+      .catch((e) => {
+        console.log("ERRORR", e);
+      });
+  }, []);
 
-  //   client
-  //     .getEntries({ content_type: "footer" })
-  //     .then((res) => {
-  //       setData(res.items);
-  //       console.log("in use effect", res);
-  //     })
-  //     .catch((e) => {
-  //       console.log("ERRORR", e);
-  //     });
-  //   tagline = data ? data[0].fields.taglinePl : "nie ma daty";
-  // }, []);
+  // console.log(data[0]?.fields.taglineEn ? data[0].fields.taglineEn : "");
+
+  if (data) {
+    tagline =
+      lang === "en" ? data[0].fields.taglineEn : data[0].fields.taglinePl;
+  }
+
+  // tagline = data ? data[0].fields.taglinePl : "";
 
   // tagline = lang === "en" ? data[0].fields.taglineEn : data[0].fields.taglinePl;
-
-  // console.log("wyświetlam w layout 2");
-  // console.log(tagline);
-
-  // let tagline;
-
-  // let tagline = data ? data[0].fields.taglinePl : "nie ma daty";
 
   return (
     <>
