f1f4bb245656fe4c3dfa5a195e59b5288823b0fe
Dariusz Stawik <dariusz.stawik@gmail.com>
2023-06-28 11:27:31 +0200

    Fix error

--- pages/information-for-homeless.tsx
+++ pages/information-for-homeless.tsx
@@ -134,7 +134,7 @@ const informationForHomeless = ({
           {lang === "en" && titleEn ? titleEn : titlePl}
         </TitleWithDevider>
         <Image
-          src={`https://${image.fields.file.url}`}
+          src={`https:${image.fields.file.url}`}
           width={image.fields?.file?.details?.image?.width}
           height={image.fields?.file?.details?.image?.height}
           className="w-full object-cover rounded-lg my-10"
